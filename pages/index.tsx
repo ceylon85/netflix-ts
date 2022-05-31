0b94a27ee5f465f34e7c0da0b869ed670ef9072c
ceylon85 <canleez123@gmail.com>
2022-05-31 17:25:30 +0900

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix App_TS</title>
       </Head>
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         {/* Banner */}
         <section>
           {/* Row */}
-          
+
         </section>
       </main>
       <footer className="flex items-center justify-center w-full h-24 border-t">
