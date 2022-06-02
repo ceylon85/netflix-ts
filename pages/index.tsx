4c2e7055b8e4b22e196603afe58ca82b29c6c4ab
ceylon85 <canleez123@gmail.com>
2022-06-02 16:48:47 +0900

    banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  // topRated: Movie[]
+  // actionMovies: Movie[]
+  // comedyMovies: Movie[]
+  // animation: Movie[]
+  // fantasyMovies: Movie[]
+  // familyMovies: Movie[]
+  // romanceMovies: Movie[]
+  // documentaries: Movie[]
+  // mysteryMovies: Movie[]
+  // historyMovies: Movie[]
 
-const Home: NextPage = () => {
+}
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+
+  // topRated,
+  // actionMovies,
+  // comedyMovies,
+  // animation,
+  // fantasyMovies,
+  // familyMovies,
+  // romanceMovies,
+  // documentaries,
+  // mysteryMovies,
+  // historyMovies,
+}: Props) => {
+  //console.log(netflixOriginals, trendingNow);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -12,7 +41,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* Row */}
 
