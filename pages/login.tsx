20e1d6271a47dd3fe4d81458f74c16db4c111543
ceylon85 <canleez123@gmail.com>
2022-06-10 19:49:03 +0900

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,25 +1,40 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import React from 'react'
 
 function Login() {
   return (
-    <div className='relative flex flex-col w-screen h-screen bg-black md:items-center md:justify-center md:bg-transparent'>
-      <Head>
-        <title>Netflix App</title>
-      </Head>
-      <img
-        className="-z-10 !hidden opacity-60 sm:!inline bg-cover"
+    <div className='relative flex flex-col w-screen h-screen bg-black md:items-center md:justify-center md:bg-transparent '>
+
+      <Image
         src="https://assets.nflxext.com/ffe/siteui/vlv3/92bb3a0b-7e91-40a0-b27b-f2c3ac9ef6e4/180ca992-4d7b-4274-99be-cf8169176af2/KR-ko-20210322-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-        alt="bg"
+        className="opacity-80 sm:inline"
+        alt="bg_cover"
+        objectFit='cover'
+        layout='fill'
       />
-      <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="main logo" height={150} width={150} className="absolute object-contain cursor-pointer left-4 top-4 md:left-10 md:top-6 "
-      />
-      <form action=""><h1>Sign In</h1>
-        <div><label htmlFor="">
-          <input type="email" placeholder='Email' /></label>
-          <label></label></div>
-      </form>
+      <div className='relative z-[1] w-full h-[100vh] bg-black/40 bg-gradient-to-t'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="main logo" height={100} width={150} className="absolute object-contain cursor-pointer left-4 top-4 md:left-10 md:top-6 "
+        />
+
+        <form className="relative max-w-[400px] max-y-[600px] px-10 py-10 mt-40 space-y-8 rounded bg-black/75  md:px-14 md:max-w-[500px] mx-auto ">
+          <h1 className='text-2xl font-bold'>로그인</h1>
+          <div className='space-y-3'>
+            <label className='inline-block w-full' >
+              <input type="email" placeholder='Email' className='input' />
+            </label>
+            <label className='inline-block w-full'>
+              <input type="password" placeholder='PassWord' className='input' />
+            </label>
+
+          </div>
+
+          <button className='bg-red-700 input'>로그인</button>
+
+        </form>
+      </div>
     </div>
+
 
   )
 }
