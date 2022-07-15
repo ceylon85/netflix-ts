7b5a4b34de031368443fc4d0e3375325b6ec7015
ceylon85 <canleez123@gmail.com>
2022-07-15 19:35:04 +0900

    plans

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,18 +1,33 @@
 import Head from "next/head"
+import Link from "next/link"
+import useAuth from "../hooks/useAuth"
 
 
 function Plans() {
+    const { logout, user } = useAuth()
+
     return (
         <div>
             <Head>
                 <title>Netflix App_TS</title>
             </Head>
-            <header>
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="main logo" height={100} width={100} className="object-contain cursor-pointer " />
-            <div>logout</div>
-        </div>
+            <header className="border-b border-white/10 bg=[#141414]">
+                <Link href="/">
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                        alt="main logo"
+                        height={100}
+                        width={100}
+                        className="object-contain cursor-pointer " />
+                </Link>
+                <button
+                    className="text-lg font-medium hover:underline"
+                    onClick={logout}>Logout </button>
             </header>
+
+            <main>
+                <h1 className="mb-3 text-3xl font-medium">Choose the plan that's right for you</h1>
+            </main>
         </div >
     )
 }
