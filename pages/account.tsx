ca3a382b33a1c131577f6e3ba622f96dfcbb94b9
ceylon85 <canleez123@gmail.com>
2022-08-12 18:07:39 +0900

    account

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,15 +1,30 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Header from '../components/Header'
-
+import useAuth from '../hooks/useAuth'
+import useSubscription from '../hooks/useSubscription'
+ 
 function Account() {
+    const {user}  = useAuth()
+    const subscription = useSubscription(user)
+
     return (
         <div>
             <Head>
                 <title>계정 설정</title>
             </Head>
             <Header />
-          
+          <main className='pt-20'> 
+            <div>
+                <h1 className='text-3xl md:text-4xl'>
+                    계정 설정
+                </h1>
+                <div>
+                    <img src="" alt="" />
+                    <p>다음 결제 날짜는 {subscription?.created}입니다</p>
+                </div>
+            </div>
+          </main>
         </div>
     )
 }
