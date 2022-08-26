e8d2faa9b487a236275f4b91443e0621aa8192d2
ceylon85 <canleez123@gmail.com>
2022-08-26 19:37:42 +0900

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,9 @@ function Header() {
     return (
         <header className={`${isScrolled && "bg-black"}`}>
             <div className="flex items-center space-x-2 md:space-x-10">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="main logo" height={100} width={100} className="object-contain cursor-pointer " />
+                <Link href="/" >
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="main logo" height={100} width={100} className="object-contain cursor-pointer "/>
+                </Link>
 
                 <BasicMenu />
                 <ul className="hidden space-x-4 md:flex">
